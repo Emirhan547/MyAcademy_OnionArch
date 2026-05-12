5f77e960be3b4855a502d030011ebb2eca2cdd5e
Emirhan Hacıoğlu <emirhanhacioglu0@gmail.com>
2026-05-12 18:12:08 +0300

    Make dashboard chart components dynamic and fix layout nesting

--- OnionApp.WebUI/Dtos/DashboardStatsDtos/DashboardStatsDto.cs
+++ OnionApp.WebUI/Dtos/DashboardStatsDtos/DashboardStatsDto.cs
@@ -1,12 +1,17 @@
-﻿namespace OnionApp.WebUI.Dtos.DashboardStatsDtos
+namespace OnionApp.WebUI.Dtos.DashboardStatsDtos
 {
     public class DashboardStatsDto
     {
         public int CarCount { get; set; }
         public int LocationCount { get; set; }
         public int BrandCount { get; set; }
-        public string AvgDaily { get; set; }
+        public string AvgDaily { get; set; } = "0.00";
         public int TodayReservationCount { get; set; }
+        public int PendingReservationCount { get; set; }
+        public int ActiveReservationCount { get; set; }
+        public int TodayCheckOutCount { get; set; }
+        public int TodayReturnCount { get; set; }
+        public decimal ReservationLoadRate { get; set; }
+        public DateTime GeneratedAt { get; set; }
     }
 }
-
--- OnionApp.WebUI/ViewComponents/DashboardViewComponents/_AdminDashboardChart1ComponentPartial.cs
+++ OnionApp.WebUI/ViewComponents/DashboardViewComponents/_AdminDashboardChart1ComponentPartial.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OnionApp.WebUI.Models;
+using OnionApp.WebUI.Services.StatisticsServices;
 
 namespace OnionApp.WebUI.ViewComponents.DashboardViewComponents
 {
-    public class _AdminDashboardChart1ComponentPartial:ViewComponent
+    public class _AdminDashboardChart1ComponentPartial(IStatisticsService statisticsService) : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        public async Task<IViewComponentResult> InvokeAsync()
         {
-            return View();
+            var model = new DashboardViewModel
+            {
+                AvgDailyPrice = (await statisticsService.GetAvgRentPriceForDaily()).Data,
+                AvgWeeklyPrice = (await statisticsService.GetAvgRentPriceForWeekly()).Data,
+                AvgMonthlyPrice = (await statisticsService.GetAvgRentPriceForMonthly()).Data
+            };
+
+            return View(model);
         }
     }
 }
